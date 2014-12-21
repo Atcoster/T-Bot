fe8a2fb3baece6ea71c411a8979cfabeffa5e4ad
hopeIndustries <hope_industries@hotmail.com>
2014-12-21 17:29:24 +0100

    Revert "Very much changed , do not remember what but it works :')"
    This reverts commit 8623f3544bed61a95caf5b94987138ab516d97fa.
    
    Conflicts:
    	game.lua

--- game.lua
+++ game.lua
@@ -6,6 +6,17 @@ gameView = ViewControl.create()
 gameView:loadMainMenu()
 --gameView:loadLevelSelectMenu()
 --gameView:loadLevel()
+
+
+
+--reset_user_data()
+
+print(user_data.levelData["level1"]["status"])
+print(user_data.levelData["level2"]["status"])
+print(user_data.levelData["level3"]["status"])
+print(user_data.levelData["level4"]["status"])
+print(user_data.levelData["level5"]["status"])
+
   
 ----------------------------------
 -- Game loop
